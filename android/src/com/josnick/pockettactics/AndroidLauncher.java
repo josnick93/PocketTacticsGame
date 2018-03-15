c624c5577a70537477423a6e87a4a07a6eb52630
Jose Angel Caudevilla <josecaudevilla93@gmail.com>
2018-03-15 20:17:29 +0100

    First commit: proyect structure

---
+++ android/src/com/josnick/pockettactics/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.josnick.pockettactics;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.josnick.pockettactics.PocketTacticsGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PocketTacticsGame(), config);
+	}
+}
---
+++ desktop/src/com/josnick/pockettactics/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.josnick.pockettactics.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.josnick.pockettactics.PocketTacticsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PocketTacticsGame(), config);
+	}
+}
